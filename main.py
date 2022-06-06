bd9085596359251806cb4ccfbb220f1f816e9786
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-06 13:45:43 +0000

    Adding integers and Floats

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #Data Types
 
 #Strings
+
 "Hello"
-print("Hello"[4])#Prints  'o'
+print("Hello"[4]) #Prints  'o'
+print("123" + "345") #Concatenated
+
 
 #Integers
+
 123
+
+#Float
+
+3.14159
+
+#Boolean
+
 
